b6137cb7a7286f7922e3f4bc005dbe1be3fbf7a5
MateusSantosFonseca <mateus.santos.fsc@gmail.com>
2020-06-21 05:18:55 -0300

    Alterado source da imagem de reset para corrigir bug do deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
 
   firebase: {
-    apiKey: "key",
+    apiKey: "AIzaSyCkDX2T9PNDmA_pyr8A4uiOD_qigXji5CA",
     authDomain: "dragon-timer.firebaseapp.com",
     databaseURL: "https://dragon-timer.firebaseio.com",
     projectId: "dragon-timer",
