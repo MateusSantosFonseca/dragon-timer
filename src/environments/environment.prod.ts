85a64c7208f6ced3c61ddd9eb46192a61cb7ddb8
MateusSantosFonseca <mateus.santos.fsc@gmail.com>
2020-06-21 22:46:36 -0300

    Alterado environmente prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
 
   firebase: {
-    apiKey: "AIzaSyCkDX2T9PNDmA_pyr8A4uiOD_qigXji5CA",
-    authDomain: "dragon-timer.firebaseapp.com",
-    databaseURL: "https://dragon-timer.firebaseio.com",
-    projectId: "dragon-timer",
-    storageBucket: "dragon-timer.appspot.com",
-    messagingSenderId: "187633245016",
-    appId: "1:187633245016:web:a3384b2a22c2736c8ca32d",
-    measurementId: "G-W9679EDNN9"
+    apiKey: "key",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
   }
 };
